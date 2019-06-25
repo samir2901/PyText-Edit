2ed1279b121fe65a39f7406f9d1c5f1bb902c265
Samiruddin Thunder <48079552+samir2901@users.noreply.github.com>
2019-06-25 11:29:01 +0530

    Update

--- pytextedit.py
+++ pytextedit.py
@@ -1,3 +1,8 @@
+'''
+PyText Exit ver 1.0
+Developed by Samiruddin Thunder.
+June 25, 2019
+'''
 from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox
@@ -5,8 +10,6 @@ from tkinter import messagebox
 filename = None
 
 def newFile():
-    global filename
-    filename = "Untitled"
     text.delete(0.0, END)
     print("New File Made")
 
@@ -53,15 +56,34 @@ def exitEditor():
 def viewHelp():
     helpWindow=Toplevel()
     helpWindow.title("Help-PyText Edit")
-    helpWindow.geometry("300x300")
+    helpWindow.geometry("300x110")
     helpWindow.iconbitmap("Help Logo.ico")
+    helpWindow.config(background="yellow")
+    helpLabel = Label(helpWindow,text="Help", font=("Britannic Bold", 20), bg="yellow").pack()
+    details_help = Label(helpWindow,
+        text="In this PyText Edit you can type anything\nSave it in any format\nOpen files in any format.",
+        font=("Helvetica",12),
+        bg="yellow",
+    ).pack()
     print("Help Window Opened")
 
 def viewAbout():
     aboutWindow=Toplevel()
     aboutWindow.title("About-PyText Edit")
-    aboutWindow.geometry("300x300")
+    aboutWindow.geometry("300x190")
     aboutWindow.iconbitmap("About Logo.ico")
+    aboutWindow.config(background="yellow")
+    aboutLabel = Label(aboutWindow,text="About", font=("Britannic Bold", 20), bg="yellow").pack()
+    details1_about = Label(aboutWindow,
+        text="PyText Edit ver 1.0",
+        font=("Helvetica",9),
+        bg="yellow"
+    ).pack()
+    details2_about = Label(aboutWindow,
+        text="PyText Edit is made using Python\nand its tkinter library.\nDeveloped by Samiruddin Thunder.\n\nAll of its source code is available at\n\'https://github.com/samir2901/PyText-Edit'.",
+        font=("Helvetica",12),
+        bg="yellow"
+    ).pack()
     print("About Window Opened")
 
 
@@ -95,7 +117,7 @@ menubar.add_cascade(label="Help", menu=help_menu)
 text=Text(root)
 text.pack(expand=True, fill=BOTH)
 
-scrollbar_y = Scrollbar(text)
+scrollbar_y = Scrollbar(text, orient='vertical')
 text.configure(yscrollcommand=scrollbar_y.set)
 scrollbar_y.config(command=text.yview)
 scrollbar_y.pack(side=RIGHT, fill=Y)
